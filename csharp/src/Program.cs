4cb37e6c5bcefb925d883b1c9be99764065901b3
Farzon Lotfi <1802579+farzonl@users.noreply.github.com>
2023-01-22 19:10:02 -0500

    add a sphere

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -2,37 +2,58 @@
 using System.Text;
 using raytracing.DataStructures;
 
-namespace raytracing {
-class Program {
+namespace raytracing
+{
+    class Program
+    {
 
-    public static Vec3<float> color(Ray<float> r) {
-        Vec3<float> unitDirection = r.direction().unitVector();
-        float t = 0.5f * unitDirection.y + 1.0f;
-        return Vec3<float>.genVector(1.0f) * (1.0f - t) + Vec3<float>.genVector(0.5f, 0.7f, 1.0f) * t;
-    }
-    static void Main(string[] args) {
-        int nx = 200;
-        int ny = 100;
-        StringBuilder output = new();
-        output.AppendLine($"P3\n{nx} {ny}\n255\n");
-        Vec3<float> lowerLeftCorner = Vec3<float>.genVector(-2.0f, -1.0f, -1.0f);
-        Vec3<float> horizontal = Vec3<float>.genVector(4.0f, 0.0f, 0.0f);
-        Vec3<float> vertical = Vec3<float>.genVector(0.0f, 2.0f, 0.0f);
-        Vec3<float> origin = Vec3<float>.genVector(0.0f, 0.0f, 0.0f);
-
-        for (int j = ny-1; j >= 0; j--) {
-            for(int i = 0; i < nx; i++) {
-                float u = (float)i / (float) nx;
-                float v = (float)j / (float) ny;
-                Ray<float> r = new Ray<float>(origin, lowerLeftCorner + horizontal * u + vertical * v);
-                Vec3<float> col = color(r);
-                int ir = (int)(255.99*col.r);
-                int ig = (int)(255.99*col.g);
-                int ib = (int)(255.99*col.b);
-                output.AppendLine($"{ir} {ig} {ib}\n");
+        public static Vec3<float> color(Ray<float> r)
+        {
+            if(hitSphere(Vec3<float>.genVector(0,0,-1),.5f,r)) {
+                return Vec3<float>.genVector(1, 0, 0);
             }
+
+            Vec3<float> unitDirection = r.direction().unitVector();
+            float t = 0.5f * unitDirection.y + 1.0f;
+            return Vec3<float>.genVector(1.0f) * (1.0f - t) + Vec3<float>.genVector(0.5f, 0.7f, 1.0f) * t;
         }
-        Console.WriteLine(output.ToString());
-    }
+
+        public static bool hitSphere(Vec3<float> center, float radius, Ray<float> r)
+        {
+            Vec3<float> oc = r.origin() - center;
+            float a = r.direction().dot();
+            float b = 2.0f * r.direction().dot(oc);
+            float c = oc.dot() - (float)Math.Pow(radius, 2);
+            float discriminant = (float)Math.Pow(b, 2) - 4 * a * c;
+            return discriminant > 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int nx = 200;
+            int ny = 100;
+            StringBuilder output = new();
+            output.AppendLine($"P3\n{nx} {ny}\n255\n");
+            Vec3<float> lowerLeftCorner = Vec3<float>.genVector(-2.0f, -1.0f, -1.0f);
+            Vec3<float> horizontal = Vec3<float>.genVector(4.0f, 0.0f, 0.0f);
+            Vec3<float> vertical = Vec3<float>.genVector(0.0f, 2.0f, 0.0f);
+            Vec3<float> origin = Vec3<float>.genVector(0.0f, 0.0f, 0.0f);
+
+            for (int j = ny - 1; j >= 0; j--)
+            {
+                for (int i = 0; i < nx; i++)
+                {
+                    float u = (float)i / (float)nx;
+                    float v = (float)j / (float)ny;
+                    Ray<float> r = new Ray<float>(origin, lowerLeftCorner + horizontal * u + vertical * v);
+                    Vec3<float> col = color(r);
+                    int ir = (int)(255.99 * col.r);
+                    int ig = (int)(255.99 * col.g);
+                    int ib = (int)(255.99 * col.b);
+                    output.AppendLine($"{ir} {ig} {ib}\n");
+                }
+            }
+            Console.WriteLine(output.ToString());
+        }
     }
 }
--- csharp/src/Vector.cs
+++ csharp/src/Vector.cs
@@ -68,9 +68,9 @@ namespace raytracing
                 this.z = z;
             }
 
-            public Vec3(T a) : this(a,a,a)
+            public Vec3(T a) : this(a, a, a)
             {
-               
+
             }
 
             public static Vec3<T> genVector(T x, T y, T z)
@@ -90,10 +90,17 @@ namespace raytracing
 
             public static Vec3<T> Zero() { return new Vec3<T>(T.Zero, T.Zero, T.Zero); }
 
-            public static double Pow(T n, double p) => Math.Pow((double)(object)n, p);
+            public static double Pow(T n, double p) => Math.Pow((double)new decimal((float)(object)n), p);
 
-            public T length() => (T)(object)Math.Sqrt(Pow(x, 2) + Pow(y, 2) + Pow(z, 2));
 
+            public T length() {
+                double len = Math.Sqrt((Pow(x, 2) + Pow(y, 2) + Pow(z, 2)));
+                Type t = typeof(T);
+                if(t == typeof(float) || t == typeof(int)){
+                    return (T)(object)Convert.ToSingle(len);
+                }
+                return (T)(object)len; 
+            }
             public static Vec3<T> operator -(Vec3<T> a) => new Vec3<T>(-a.x, -a.y, -a.z);
 
             public static Vec3<T> operator +(Vec3<T> a, Vec3<T> b)
@@ -111,6 +118,21 @@ namespace raytracing
             public static Vec3<T> operator *(Vec3<T> a, T n) => new Vec3<T>(a.x * n, a.y * n, a.z * n);
 
             public static Vec3<T> operator /(Vec3<T> a, T n) => new Vec3<T>(a.x / n, a.y / n, a.z / n);
+
+            public static T dot(Vec3<T> a, Vec3<T> b)
+            {
+                return a.arr[0] * b.arr[0] + a.arr[1] * b.arr[1] + a.arr[2] * b.arr[2];
+            }
+
+            public T dot(Vec3<T> a)
+            {
+                return Vec3<T>.dot(this, a);
+            }
+
+            public T dot()
+            {
+                return Vec3<T>.dot(this, this);
+            }
         }
     }
 }
